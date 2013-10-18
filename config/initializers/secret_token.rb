2f5eac37f332d35b362af44e161ea3972c3ae593
Aleksandar Terentic <aterentic@gmail.com>
2013-10-18 23:36:04 +0200

    Added initial memoro project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Memoro::Application.config.secret_key_base = '112f0bff33775a81d4e023438801367292c93e6df81dfe2d827885c08274ffef25e38f7328e488604894f7eedc630d5931dfe25817beb9cd82fc8d7cb1b7b8b6'
