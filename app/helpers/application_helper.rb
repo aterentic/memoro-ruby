08c151ff8f11c0229a3b602a04e6abc468ab6626
Aleksandar Terentic <aterentic@gmail.com>
2013-10-20 13:44:04 +0200

    Added customized page title with default value.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def page_title(page_title)
+    base_title = "Memoro"
+    if page_title.empty?
+			base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
--- spec/requests/abouts_spec.rb
+++ spec/requests/abouts_spec.rb
@@ -1,15 +1,21 @@
 require 'spec_helper'
 
 describe "About" do
-  describe "GET /about" do
+  describe "GET" do
     it "should have the content 'Memoro'" do
       visit '/about'
       expect(page).to have_content('Memoro')
+    end
+  end
+
+  describe "GET" do
+		it "should have the content 'Complementing your brain since 2013.'" do
+      visit '/about'
       expect(page).to have_content('Complementing your brain since 2013.')
     end
   end
 
-  describe "GET /about" do
+  describe "GET" do
     it "should have title 'Memoro | About'" do
       visit '/about'
       expect(page).to have_title('Memoro | About')
