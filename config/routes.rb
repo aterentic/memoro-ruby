061d772df00343b4226e32bf283c96c759dff9b2
Aleksandar Terentic <aterentic@gmail.com>
2013-10-21 20:32:09 +0200

    Updated about page routing.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Memoro::Application.routes.draw do
   resources :users
-  get "about" => "about"
+  match "/about", { to: "about#about", via: "get" }
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- spec/requests/abouts_spec.rb
+++ spec/requests/abouts_spec.rb
@@ -3,21 +3,21 @@ require 'spec_helper'
 describe "About" do
   describe "GET" do
     it "should have the content 'Memoro'" do
-      visit '/about'
+      visit about_path
       expect(page).to have_content('Memoro')
     end
   end
 
   describe "GET" do
 		it "should have the content 'Complementing your brain since 2013.'" do
-      visit '/about'
+      visit about_path
       expect(page).to have_content('Complementing your brain since 2013.')
     end
   end
 
   describe "GET" do
     it "should have title 'Memoro | About'" do
-      visit '/about'
+      visit about_path
       expect(page).to have_title('Memoro | About')
     end
   end
