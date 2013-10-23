3c7c8e735e4b40c75421e33f643b953918c69620
Aleksandar Terentic <aterentic@gmail.com>
2013-10-23 23:07:02 +0200

    Added uniqueness to users email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :email, presence: true, length: { maximum: 254 }, format: { with: VALID_EMAIL_REGEX }
+	validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 254 }, format: { with: VALID_EMAIL_REGEX }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131023191323) do
+ActiveRecord::Schema.define(version: 20131023210228) do
 
   create_table "users", force: true do |t|
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
